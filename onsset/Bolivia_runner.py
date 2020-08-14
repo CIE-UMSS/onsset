d23ac79e32af1c123ab1dbc85a954093d899f275
unknown <you@example.com>
2020-08-14 14:49:53 -0400

    14/08/2020 214/08/2020 2

--- onsset/Bolivia_runner.py
+++ onsset/Bolivia_runner.py
@@ -13,19 +13,3 @@ summary_folder	= os.path.join('Bolivia')
 scenario(specs_path, calibrated_csv_path, results_folder, summary_folder)
     
 data = pd.read_csv('Bolivia/bo-1-0_0_0_0_0_0.csv', index_col=0)  
-df = pd.DataFrame()
-df['MG_Hybrid_ChacoFuelCost2020'] = data['MG_Hybrid_ChacoFuelCost2020']
-df['MG_Hybrid_Chaco2020'] = data['MG_Hybrid_Chaco2020']
-df['MG_Hybrid_Chaco2030'] = data['MG_Hybrid_Chaco2030']
-df['InvestmentCost2020'] = data['InvestmentCost2020']
-df['InvestmentCost2030'] = data['InvestmentCost2030']
-df['MinimumOverall2020'] = data['MinimumOverall2020']
-df['MinimumOverallLCOE2020'] = data['MinimumOverallLCOE2020']
-df['NewCapacity2020'] =  data['NewCapacity2020']
-df['NewCapacity2030'] =  data['NewCapacity2030']
-df['PVcapacity2020'] =  data['PVcapacity2020']
-df['PVcapacity2030'] =  data['PVcapacity2030']
-df['GenSetcapacity2020'] =  data['GenSetcapacity2020']
-df['GenSetcapacity2030'] =  data['GenSetcapacity2030']
-df['Batterycapacity2020'] =  data['Batterycapacity2020']
-df['Batterycapacity2030'] =  data['Batterycapacity2030']
